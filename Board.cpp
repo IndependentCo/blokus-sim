12c4dfd685a91055cac6286033f1a69ddb6085a1
MHanlon148 <33841750+MHanlon148@users.noreply.github.com>
2017-11-27 14:39:18 -0500

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,7 +1,6 @@
 //Board.cpp
 
 #include <iostream>
-#include "Piece.cpp"
 #include <vector>
 #include "Board.hpp"
 
@@ -15,17 +14,6 @@ Board::Board(){
   }
 }
 
-int Board::placePiece(Piece piece, int x, int y, int player){
-  vector<vector<int>> squares = piece.getCoords();
-  for(int i=0;i<squares.size();i++){
-    vector<int> currsquare = squares[i];
-    if(x+currpiece[0] >=20 || y+currpiece[1] >=20)
-      return 1;
-    boardmatrix[x+currpiece[0]][y+currpiece[1]] = player;
-  }
-  return 0;
-}
-
 void Board::print(){
   for(int i=0;i<20;i++){
     for(int j=0;j<20;j++){
@@ -35,7 +23,7 @@ void Board::print(){
   }
 }
 
-Board::isLegalMove(Piece piece, int x, int y, int player) {
+bool Board::isLegalMove(Piece piece, int x, int y, int player) {
     vector<vector<int>> squares = piece.getCoords();
     for(int i = 0, i < squares.size(); i++) {
         vector<int> currsquare = squares[i];
